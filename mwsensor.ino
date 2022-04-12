924c657a3d5f5efb0339ae3768e912abaa81bfce
mograine666 <89299628+mograine666@users.noreply.github.com>
2022-04-12 15:51:37 +0600

    Update mwsensor.ino

--- mwsensor.ino
+++ mwsensor.ino
@@ -1,8 +1,10 @@
-#include <SPI.h>
+ #include <SPI.h>
 #include <nRF24L01.h>
+#include "SD.h"
+#define SD_ChipSelectPin 4
+#include "TMRpcm.h"
 #include <RF24.h>
-
-#define button 4
+TMRpcm tmrpcm;
 //global parametres
 RF24 radio(7, 8); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
@@ -12,14 +14,14 @@ bool activ=false;//Состояние системы сигнализации
 bool flag=false;//Состояние зарядки
 bool fl=false;//Состояние LED знака
 const int hlim=100;//Верхний лимит зарядки
-int mx_time=5000;//Значение задержки таймера
+int mx_time=10000;//Значение задержки таймера
 int ledPin=13;//Вывод на проектор глобо
 int drivePin=12;//Вывод на знак для водителей
 int soundPin=11;//Вывод на звук для пешеходов
 int batPin=10;//Вывод на батарею контроллера
 int GOBOPin=9;//Вывод на бетарею GOBO
 int hlim_GLOBO=100;//Верхний лимит зарядки GLOBO;
- unsigned long last=millis();
+unsigned long last=millis();
  
 void setup() {
   pinMode(button, INPUT);
@@ -27,7 +29,14 @@ void setup() {
   radio.openWritingPipe(addresses[0]); // 00001
   radio.openReadingPipe(1, addresses[1]); // 00002
   radio.setPALevel(RF24_PA_MIN);
- 
+  tmrpcm.speakerPin = 9;
+  Serial.begin(9600);
+  if (!SD.begin(SD_ChipSelectPin)) {
+    Serial.println("SD fail");
+    return;
+  }
+  tmrpcm.setVolume(6);
+  tmrpcm.play("rain.wav");
 }
 
 void sys_ON(){
@@ -44,12 +53,9 @@ void sys_OFF(){
 }
 
 void loop() {
-  if(activ==true && millis() - last > mx_time){
+  if(activ==true && millis() - last >= mx_time){
     last=millis();  
     sys_OFF();
-  }
-  else if( millis() - last >= mx_time){
-    last=millis();  
   }
   int bat_ch=analogRead(A1);
   int bat_ch_GLOBO=analogRead(A2);
@@ -69,36 +75,41 @@ void loop() {
     digitalWrite(GOBOPin, LOW);
     fl=false;
   }
-  radio.startListening();
-  if (radio.available()) {
-      radio.read(&buttonState, sizeof(buttonState));
-      if(buttonState==1){
-        if(activ==false)
-          sys_ON();
-        last = millis();
-      }
-  }
-  radio.stopListening();  
-  if(bat_ch<lim){
-      int potValue = analogRead(A3);
-      if(potValue==1){
-          int angleValue=map(1, 0, 1023, 0, 180);
-          if(activ==false)
+  if(activ==false){
+    radio.startListening();
+    if (radio.available()) {
+        radio.read(&buttonState, sizeof(buttonState));
+        if(buttonState==1){
             sys_ON();
-          last = millis();
-          radio.write(&angleValue, sizeof(angleValue));
-      }
+            last = millis();
+        }
+    }
+    radio.stopListening();  
+    if(bat_ch<lim){
+        int potValue = analogRead(A3);  
+        if(potValue==1){
+            int angleValue=map(1, 0, 1023, 0, 180);
+            if(activ==false){
+              sys_ON();z
+              last = millis();
+              radio.write(&angleValue, sizeof(angleValue));
+            }              
+        }
+    }
+    else{
+        int potValue = analogRead(A3);
+        int sensValue = analogRead(A4);
+        if(potValue==1 || sensValue==1){
+           int angleValue = map(1, 0, 1023, 0, 180);
+           if(activ==false){
+              sys_ON();
+              last = millis();
+              radio.write(&angleValue, sizeof(angleValue));
+            }  
+           
+        }
+    }
   }
-  else{
-      int potValue = analogRead(A3);
-      int sensValue = analogRead(A4);
-      if(potValue==1 || sensValue==1){
-         int angleValue = map(1, 0, 1023, 0, 180);
-         if(activ==false)
-          sys_ON();
-         last = millis();
-         radio.write(&angleValue, sizeof(angleValue));
-      }
-  }
+  
 
 }
