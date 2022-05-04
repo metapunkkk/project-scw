c1d028f4b0add91d9e618cfb2587d25c19bbf86b
mograine666 <89299628+mograine666@users.noreply.github.com>
2022-05-04 14:45:55 +0600

    Update mwsensor.ino

--- mwsensor.ino
+++ mwsensor.ino
@@ -1,10 +1,11 @@
- #include <SPI.h>
+#include <SPI.h>
 #include <nRF24L01.h>
 #include "SD.h"
 #define SD_ChipSelectPin 4
-#include "TMRpcm.h"
+
 #include <RF24.h>
-TMRpcm tmrpcm;
+#include <LiquidCrystal.h>
+
 //global parametres
 RF24 radio(7, 8); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
@@ -17,26 +18,28 @@ const int hlim=100;//Верхний лимит зарядки
 int mx_time=10000;//Значение задержки таймера
 int ledPin=13;//Вывод на проектор глобо
 int drivePin=12;//Вывод на знак для водителей
+
 int soundPin=11;//Вывод на звук для пешеходов
 int batPin=10;//Вывод на батарею контроллера
 int GOBOPin=9;//Вывод на бетарею GOBO
-int hlim_GLOBO=100;//Верхний лимит зарядки GLOBO;
+int hlim_LOGO=100;//Верхний лимит зарядки LOGO;
 unsigned long last=millis();
- 
+int button=5;
+ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 void setup() {
   pinMode(button, INPUT);
   radio.begin();
   radio.openWritingPipe(addresses[0]); // 00001
   radio.openReadingPipe(1, addresses[1]); // 00002
   radio.setPALevel(RF24_PA_MIN);
-  tmrpcm.speakerPin = 9;
   Serial.begin(9600);
   if (!SD.begin(SD_ChipSelectPin)) {
     Serial.println("SD fail");
     return;
   }
-  tmrpcm.setVolume(6);
-  tmrpcm.play("rain.wav");
+  
 }
 
 void sys_ON(){
@@ -58,7 +61,7 @@ void loop() {
     sys_OFF();
   }
   int bat_ch=analogRead(A1);
-  int bat_ch_GLOBO=analogRead(A2);
+  int bat_ch_LOGO=analogRead(A2);
   if(bat_ch<hlim && flag==false){
     digitalWrite(batPin, HIGH);
     flag=true;
@@ -67,11 +70,11 @@ void loop() {
     digitalWrite(batPin, LOW);
     flag=false;
   }
-  if(bat_ch_GLOBO<hlim_GLOBO && fl==false){
+  if(bat_ch_LOGO<hlim_LOGO && fl==false){
     digitalWrite(GOBOPin, HIGH);
     fl=true;
   }
-  if(bat_ch_GLOBO==hlim_GLOBO && fl==true){
+  if(bat_ch_LOGO==hlim_LOGO && fl==true){
     digitalWrite(GOBOPin, LOW);
     fl=false;
   }
@@ -90,7 +93,7 @@ void loop() {
         if(potValue==1){
             int angleValue=map(1, 0, 1023, 0, 180);
             if(activ==false){
-              sys_ON();z
+              sys_ON();
               last = millis();
               radio.write(&angleValue, sizeof(angleValue));
             }              
